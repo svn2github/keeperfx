svn2github/keeperfx

commit fe0cb74c68219a12bc7b923edc1cbae0c0f60442

Rewritten affecting creatures by alarm trap.
git-svn-id: http://keeperfx.googlecode.com/svn/trunk@1494 40a56aae-0377-11df-bfb3-7781b9cb07ea

keeperfx/src/creature_states_mood.c
@@ -25,6 +25,7 @@
 #include "thing_list.h"
 #include "creature_control.h"
 #include "creature_instances.h"
+#include "creature_states_lair.h"
 #include "config_creature.h"
 #include "config_rules.h"
 #include "config_terrain.h"
@@ -373,5 +374,17 @@ TbBool anger_make_creature_angry(struct Thing *creatng, AnnoyMotive reason)
         return false;
     anger_set_creature_anger(creatng, crstat->annoy_level, reason);
     return true;
+}
+
+TbBool creature_mark_if_woken_up(struct Thing *creatng)
+{
+    if (creature_is_sleeping(creatng))
+    {
+        struct CreatureStats *crstat;
+        crstat = creature_stats_get_from_thing(creatng);
+        anger_apply_anger_to_creature(creatng, crstat->annoy_woken_up, AngR_Other, 1);
+        return true;
+    }
+    return false;
 }
 /******************************************************************************/

keeperfx/src/magic.c
@@ -43,6 +43,7 @@
 #include "creature_control.h"
 #include "creature_states.h"
 #include "creature_states_lair.h"
+#include "creature_states_mood.h"
 #include "config_creature.h"
 #include "config_terrain.h"
 #include "config_magic.h"
@@ -447,8 +448,7 @@ void slap_creature(struct PlayerInfo *player, struct Thing *thing)
         clear_creature_instance(thing);
         cctrl->active_state_bkp = thing->active_state;
         cctrl->continue_state_bkp = thing->continue_state;
-        if (creature_is_sleeping(thing))
-            anger_apply_anger_to_creature(thing, crstat->annoy_woken_up, AngR_Other, 1);
+        creature_mark_if_woken_up(thing);
         external_set_thing_state(thing, CrSt_CreatureSlapCowers);
     }
     cctrl->field_B1 = 6;
@@ -1336,7 +1336,7 @@ long update_creatures_influenced_by_call_to_arms(PlayerNumber plyr_idx)
             {
                 struct StateInfo *stati;
                 stati = get_thing_state_info_num(get_creature_state_besides_interruptions(thing));
-                if ( stati->field_28 || creature_is_called_to_arms(thing) )
+                if (stati->field_28 || creature_is_called_to_arms(thing))
                 {
                     if (creature_can_navigate_to_with_storage(thing, &pos, NavRtF_Default))
                     {
@@ -1554,16 +1554,12 @@ int affect_nearby_creatures_by_power_call_to_arms(PlayerNumber plyr_idx, long ra
             nstat = get_creature_state_besides_interruptions(thing);
             struct StateInfo *stati;
             stati = get_thing_state_info_num(nstat);
-            if (((cctrl->spell_flags & CSAfF_CalledToArms) == 0) || (stati->field_28))
+            if (!creature_is_called_to_arms(thing) || stati->field_28)
             {
                 if (stati->field_28
-                  && (((cctrl->spell_flags & CSAfF_CalledToArms) != 0) || get_2d_box_distance(&thing->mappos, pos) < range))
+                  && (creature_is_called_to_arms(thing) || get_2d_box_distance(&thing->mappos, pos) < range))
                 {
-                    if (creature_is_sleeping(thing)) {
-                        struct CreatureStats *crstat;
-                        crstat = creature_stats_get_from_thing(thing);
-                        anger_apply_anger_to_creature(thing, crstat->annoy_woken_up, AngR_Other, 1);
-                    }
+                    creature_mark_if_woken_up(thing);
                     if (creature_can_navigate_to_with_storage(thing, pos, NavRtF_Default))
                     {
                         if (external_set_thing_state(thing, CrSt_ArriveAtCallToArms))

keeperfx/src/creature_states_hero.c
@@ -205,8 +205,6 @@ TbBool good_setup_loot_research_room(struct Thing *thing, long dngn_id)
 
 long get_wanderer_possible_targets_count_in_list(long first_thing_idx, struct Thing *wanderer)
 {
-    struct CreatureControl *cctrl;
-    struct Thing *thing;
     long victims_count;
     unsigned long k;
     long i;
@@ -216,6 +214,8 @@ long get_wanderer_possible_targets_count_in_list(long first_thing_idx, struct Th
     i = first_thing_idx;
     while (i != 0)
     {
+        struct CreatureControl *cctrl;
+        struct Thing *thing;
         thing = thing_get(i);
         TRACE_THING(thing);
         cctrl = creature_control_get_from_thing(thing);

keeperfx/src/creature_states_mood.h
@@ -48,6 +48,7 @@ void anger_set_creature_anger_f(struct Thing *creatng, long annoy_lv, AnnoyMotiv
 void anger_apply_anger_to_creature_all_types_f(struct Thing *thing, long anger, const char *func_name);
 #define anger_apply_anger_to_creature_all_types(thing, anger) anger_apply_anger_to_creature_all_types_f(thing, anger, __func__)
 TbBool anger_make_creature_angry(struct Thing *thing, AnnoyMotive reason);
+TbBool creature_mark_if_woken_up(struct Thing *creatng);
 
 short creature_moan(struct Thing *thing);
 short creature_roar(struct Thing *thing);

keeperfx/src/main.cpp
@@ -97,6 +97,7 @@
 #include "creature_graphics.h"
 #include "creature_states_rsrch.h"
 #include "creature_states_lair.h"
+#include "creature_states_mood.h"
 #include "lens_api.h"
 #include "light_data.h"
 #include "magic.h"
@@ -468,9 +469,62 @@ void affect_nearby_stuff_with_vortex(struct Thing *thing)
   _DK_affect_nearby_stuff_with_vortex(thing);
 }
 
-void affect_nearby_friends_with_alarm(struct Thing *thing)
+void affect_nearby_friends_with_alarm(struct Thing *traptng)
 {
-    _DK_affect_nearby_friends_with_alarm(thing);
+    //_DK_affect_nearby_friends_with_alarm(thing);
+    SYNCDBG(8,"Starting");
+    if (is_neutral_thing(traptng)) {
+        return;
+    }
+    struct Dungeon *dungeon;
+    unsigned long k;
+    int i;
+    dungeon = get_players_num_dungeon(traptng->owner);
+    k = 0;
+    i = dungeon->creatr_list_start;
+    while (i != 0)
+    {
+        struct CreatureControl *cctrl;
+        struct Thing *thing;
+        thing = thing_get(i);
+        TRACE_THING(thing);
+        cctrl = creature_control_get_from_thing(thing);
+        if (creature_control_invalid(cctrl))
+        {
+            ERRORLOG("Jump to invalid creature detected");
+            break;
+        }
+        i = cctrl->players_next_creature_idx;
+        // Thing list loop body
+        if (!thing_is_picked_up(thing) && !creature_is_manually_controlled_by_owner(thing) &&
+            !creature_is_being_unconscious(thing) && !creature_is_kept_in_custody(thing) &&
+            (cctrl->combat_flags == 0) && !creature_is_dragging_something(thing) && !creature_is_dying(thing))
+        {
+            struct StateInfo *stati;
+            stati = get_thing_state_info_num(get_creature_state_besides_interruptions(thing));
+            if (stati->field_28 && (get_2d_box_distance(&traptng->mappos, &thing->mappos) < 4096))
+            {
+                creature_mark_if_woken_up(thing);
+                if (external_set_thing_state(thing, CrSt_ArriveAtAlarm))
+                {
+                    if (setup_person_move_to_position(thing, traptng->mappos.x.stl.num, traptng->mappos.y.stl.num, 0))
+                    {
+                        thing->continue_state = CrSt_ArriveAtAlarm;
+                        cctrl->field_2FA = game.play_gameturn + 800;
+                        cctrl->alarm_stl_x = traptng->mappos.x.stl.num;
+                        cctrl->alarm_stl_y = traptng->mappos.y.stl.num;
+                    }
+                }
+            }
+        }
+        // Thing list loop body ends
+        k++;
+        if (k > CREATURES_COUNT)
+        {
+            ERRORLOG("Infinite loop detected when sweeping creatures list");
+            break;
+        }
+    }
 }
 
 long apply_wallhug_force_to_boulder(struct Thing *thing)

commit 1d007fdcf614b22489fa241b802b4366391ec150

Replaced some checks by unified functions. Fixed CTA code bug introduced in r1494.
git-svn-id: http://keeperfx.googlecode.com/svn/trunk@1531 40a56aae-0377-11df-bfb3-7781b9cb07ea

keeperfx/.cproject
@@ -32,7 +32,7 @@
 						</toolChain>
 					</folderInfo>
 					<sourceEntries>
-						<entry flags="VALUE_WORKSPACE_PATH|RESOLVED" kind="sourcePath" name="src"/>
+						<entry flags="VALUE_WORKSPACE_PATH" kind="sourcePath" name="src"/>
 					</sourceEntries>
 				</configuration>
 			</storageModule>

keeperfx/src/creature_jobs.c
@@ -204,7 +204,7 @@ TbBool creature_free_for_anger_job(struct Thing *creatng)
     struct Dungeon *dungeon;
     cctrl = creature_control_get_from_thing(creatng);
     dungeon = get_dungeon(creatng->owner);
-    return ((cctrl->spell_flags & CSAfF_CalledToArms) == 0)
+    return !creature_affected_by_call_to_arms(creatng)
         && (dungeon->must_obey_turn == 0)
         && ((cctrl->spell_flags & CSAfF_Chicken) == 0)
         && !thing_is_picked_up(creatng) && !is_thing_directly_controlled(creatng);

keeperfx/src/creature_states.c
@@ -768,6 +768,25 @@ TbBool creature_is_fleeing_combat(const struct Thing *thing)
     return false;
 }
 
+/**
+ * Returns if creature is in general in CTA following state.
+ * Following CTA may be interrupted by fights and other events; if this returns true,
+ *  the creature should get back to following CTA when other activity (like battle) is finished.
+ * @param thing
+ * @return
+ */
+TbBool creature_affected_by_call_to_arms(const struct Thing *thing)
+{
+    struct CreatureControl *cctrl;
+    cctrl = creature_control_get_from_thing(thing);
+    return ((cctrl->spell_flags & CSAfF_CalledToArms) != 0);
+}
+
+/**
+ * Returns if creature is currently directly in CTA following state.
+ * @param thing
+ * @return
+ */
 TbBool creature_is_called_to_arms(const struct Thing *thing)
 {
     CrtrStateId i;
@@ -3484,7 +3503,7 @@ short person_sulk_at_lair(struct Thing *creatng)
     cctrl = creature_control_get_from_thing(creatng);
     dungeon = get_players_num_dungeon(creatng->owner);
     lairtng = thing_get(cctrl->lairtng_idx);
-    if ((cctrl->slap_turns != 0) || (dungeon->must_obey_turn != 0) || !thing_exists(lairtng)) {
+    if (creature_affected_by_slap(creatng) || (dungeon->must_obey_turn != 0) || !thing_exists(lairtng)) {
         set_start_state(creatng);
         return 0;
     }
@@ -3540,7 +3559,7 @@ short person_sulk_head_for_lair(struct Thing *creatng)
     struct Dungeon *dungeon;
     crstat = creature_stats_get_from_thing(creatng);
     dungeon = get_players_num_dungeon(creatng->owner);
-    if ((crstat->lair_size <= 0) || (cctrl->slap_turns != 0) || (dungeon->must_obey_turn != 0)) {
+    if ((crstat->lair_size <= 0) || creature_affected_by_slap(creatng) || (dungeon->must_obey_turn != 0)) {
         set_start_state(creatng);
         return 0;
     }
@@ -3557,7 +3576,7 @@ short person_sulking(struct Thing *creatng)
     cctrl = creature_control_get_from_thing(creatng);
     dungeon = get_players_num_dungeon(creatng->owner);
     lairtng = thing_get(cctrl->lairtng_idx);
-    if ((cctrl->slap_turns != 0) || (dungeon->must_obey_turn != 0) || !thing_exists(lairtng)) {
+    if (creature_affected_by_slap(creatng) || (dungeon->must_obey_turn != 0) || !thing_exists(lairtng)) {
         set_start_state(creatng);
         return 0;
     }
@@ -4156,7 +4175,7 @@ long process_work_speed_on_work_value(const struct Thing *thing, long base_val)
     val = base_val;
     if (creature_affected_by_spell(thing, SplK_Speed))
         val = 2 * val;
-    if (cctrl->slap_turns)
+    if (creature_affected_by_slap(thing))
         val = 4 * val / 3;
     if (!is_neutral_thing(thing))
     {
@@ -4394,7 +4413,7 @@ TbBool creature_free_for_sleep(const struct Thing *thing,  CrtrStateId state)
 {
     const struct CreatureControl *cctrl;
     cctrl = creature_control_get_from_thing(thing);
-    if ((cctrl->slap_turns != 0) || ((cctrl->spell_flags & CSAfF_CalledToArms) != 0))
+    if (creature_affected_by_slap(thing) || creature_affected_by_call_to_arms(thing))
         return false;
     return can_change_from_state_to(thing, thing->active_state, state);
 }
@@ -4532,7 +4551,9 @@ char creature_free_for_lunchtime(struct Thing *creatng)
 {
     struct CreatureControl *cctrl;
     cctrl = creature_control_get_from_thing(creatng);
-    return (!cctrl->slap_turns) && ((cctrl->spell_flags & (CSAfF_CalledToArms|CSAfF_Chicken)) == 0)
+    return !creature_affected_by_slap(creatng)
+        && !creature_affected_by_call_to_arms(creatng)
+        && !creature_affected_by_spell(creatng, SplK_Chicken)
         && can_change_from_state_to(creatng, creatng->active_state, CrSt_CreatureToGarden);
 }
 

keeperfx/src/creature_states.h
@@ -374,6 +374,7 @@ TbBool creature_is_at_alarm(const struct Thing *thing);
 TbBool creature_is_escaping_death(const struct Thing *thing);
 TbBool creature_is_fleeing_combat(const struct Thing *thing);
 TbBool creature_is_called_to_arms(const struct Thing *thing);
+TbBool creature_affected_by_call_to_arms(const struct Thing *thing);
 TbBool creature_is_kept_in_custody(const struct Thing *thing);
 TbBool creature_is_kept_in_custody_by_enemy(const struct Thing *thing);
 TbBool creature_is_kept_in_custody_by_player(const struct Thing *thing, PlayerNumber plyr_idx);

keeperfx/src/creature_states_lair.c
@@ -361,7 +361,7 @@ short at_lair_to_sleep(struct Thing *thing)
     lairtng = thing_get(cctrl->lairtng_idx);
     TRACE_THING(lairtng);
     cctrl->target_room_id = 0;
-    if (thing_is_invalid(lairtng) || (cctrl->slap_turns != 0))
+    if (thing_is_invalid(lairtng) || creature_affected_by_slap(thing))
     {
         set_start_state(thing);
         return 0;
@@ -450,7 +450,7 @@ short creature_sleep(struct Thing *thing)
 {
     struct CreatureControl *cctrl;
     cctrl = creature_control_get_from_thing(thing);
-    if ((cctrl->slap_turns > 0) || !creature_will_sleep(thing)) {
+    if (creature_affected_by_slap(thing) || !creature_will_sleep(thing)) {
         set_start_state(thing);
         return 0;
     }

keeperfx/src/creature_states_mood.c
@@ -216,7 +216,7 @@ TbBool anger_free_for_anger_increase(struct Thing *creatng)
     if (cctrl->combat_flags != 0) {
         return false;
     }
-    return (cctrl->spell_flags & CSAfF_CalledToArms) == 0;
+    return !creature_affected_by_call_to_arms(creatng);
 }
 
 TbBool anger_free_for_anger_decrease(struct Thing *creatng)

keeperfx/src/creature_states_tortr.c
@@ -480,7 +480,7 @@ long compute_torture_convert_time(const struct Thing *thing, const struct Room *
     i = ((long)game.play_gameturn - cctrl->tortured.start_gameturn) * room->efficiency / ROOM_EFFICIENCY_MAX;
     if (creature_affected_by_spell(thing, SplK_Speed))
       i = (4 * i) / 3;
-    if (cctrl->slap_turns != 0)
+    if (creature_affected_by_slap(thing))
       i = (5 * i) / 4;
     return i;
 }

keeperfx/src/magic.c
@@ -1306,10 +1306,10 @@ long update_creatures_influenced_by_call_to_arms(PlayerNumber plyr_idx)
     struct Dungeon *dungeon;
     SYNCDBG(8,"Starting");
     dungeon = get_players_num_dungeon(plyr_idx);
-    struct Coord3d pos;
-    pos.x.val = subtile_coord_center(dungeon->cta_stl_x);
-    pos.y.val = subtile_coord_center(dungeon->cta_stl_y);
-    pos.z.val = get_floor_height_at(&pos);
+    struct Coord3d cta_pos;
+    cta_pos.x.val = subtile_coord_center(dungeon->cta_stl_x);
+    cta_pos.y.val = subtile_coord_center(dungeon->cta_stl_y);
+    cta_pos.z.val = get_floor_height_at(&cta_pos);
     long count;
     count = 0;
     unsigned long k;
@@ -1332,17 +1332,17 @@ long update_creatures_influenced_by_call_to_arms(PlayerNumber plyr_idx)
         // Thing list loop body
         if (!thing_is_picked_up(thing))
         {
-            if (!creature_is_being_unconscious(thing) && ((cctrl->spell_flags & CSAfF_CalledToArms) != 0))
+            if (!creature_is_being_unconscious(thing) && creature_affected_by_call_to_arms(thing))
             {
                 struct StateInfo *stati;
                 stati = get_thing_state_info_num(get_creature_state_besides_interruptions(thing));
                 if (stati->react_to_cta || creature_is_called_to_arms(thing))
                 {
-                    if (creature_can_navigate_to_with_storage(thing, &pos, NavRtF_Default))
+                    if (creature_can_navigate_to_with_storage(thing, &cta_pos, NavRtF_Default))
                     {
                         if (creature_is_called_to_arms(thing))
                         {
-                            setup_person_move_to_coord(thing, &pos, NavRtF_Default);
+                            setup_person_move_to_coord(thing, &cta_pos, NavRtF_Default);
                             thing->continue_state = CrSt_ArriveAtCallToArms;
                             if ((cctrl->flgfield_1 & CCFlg_NoCompControl) != 0) {
                                 WARNLOG("The %s index %d is re-called to arms with no comp control, fixing",thing_model_name(thing),(int)thing->index);
@@ -1352,7 +1352,7 @@ long update_creatures_influenced_by_call_to_arms(PlayerNumber plyr_idx)
                         } else
                         if (external_set_thing_state(thing, CrSt_ArriveAtCallToArms))
                         {
-                            setup_person_move_to_coord(thing, &pos, NavRtF_Default);
+                            setup_person_move_to_coord(thing, &cta_pos, NavRtF_Default);
                             thing->continue_state = CrSt_ArriveAtCallToArms;
                             if ((cctrl->flgfield_1 & CCFlg_NoCompControl) != 0) {
                                 WARNLOG("The %s index %d is first called to arms with no comp control, fixing",thing_model_name(thing),(int)thing->index);
@@ -1554,10 +1554,10 @@ int affect_nearby_creatures_by_power_call_to_arms(PlayerNumber plyr_idx, long ra
             nstat = get_creature_state_besides_interruptions(thing);
             struct StateInfo *stati;
             stati = get_thing_state_info_num(nstat);
-            if (!creature_is_called_to_arms(thing) || stati->react_to_cta)
+            if (!creature_affected_by_call_to_arms(thing) || stati->react_to_cta)
             {
                 if (stati->react_to_cta
-                  && (creature_is_called_to_arms(thing) || get_2d_box_distance(&thing->mappos, pos) < range))
+                  && (((cctrl->spell_flags & CSAfF_CalledToArms) != 0) || get_2d_box_distance(&thing->mappos, pos) < range))
                 {
                     creature_mark_if_woken_up(thing);
                     if (creature_can_navigate_to_with_storage(thing, pos, NavRtF_Default))

keeperfx/src/player_comptask.c
@@ -2488,7 +2488,7 @@ struct Thing *find_creature_for_defend_pickup(struct Computer2 *comp)
             {
                 if (!creature_is_fleeing_combat(thing) && !creature_is_at_alarm(thing))
                 {
-                    if (!creature_affected_by_spell(thing,SplK_Chicken))
+                    if (!creature_is_being_unconscious(thing) && !creature_affected_by_spell(thing, SplK_Chicken))
                     {
                         if (!creature_is_doing_lair_activity(thing) && !creature_is_being_dropped(thing))
                         {
@@ -2666,7 +2666,7 @@ long task_slap_imps(struct Computer2 *comp, struct ComputerTask *ctask)
             i = cctrl->players_next_creature_idx;
             // Per-thing code
             // Don't slap if picked up or affected by speed or already slapped
-            if (!thing_is_picked_up(thing) && !thing_affected_by_spell(thing, SplK_Speed) && (cctrl->slap_turns == 0))
+            if (!thing_is_picked_up(thing) && !thing_affected_by_spell(thing, SplK_Speed) && !creature_affected_by_slap(thing))
             {
                 // Check if we really can use the spell on that creature, considering its position and state
                 if (can_cast_spell(dungeon->owner, PwrK_SLAP, thing->mappos.x.stl.num, thing->mappos.y.stl.num, thing, CastChk_Default))

keeperfx/src/power_specials.c
@@ -30,6 +30,7 @@
 #include "dungeon_data.h"
 #include "creature_control.h"
 #include "creature_states.h"
+#include "power_hand.h"
 #include "game_saves.h"
 #include "game_merge.h"
 #include "slab_data.h"
@@ -506,14 +507,11 @@ void resurrect_creature(struct Thing *boxtng, PlayerNumber owner, ThingModel crm
 void transfer_creature(struct Thing *boxtng, struct Thing *transftng, unsigned char plyr_idx)
 {
     SYNCDBG(7,"Starting");
-    struct Dungeon *dungeon;
     struct CreatureControl *cctrl;
-    long i;
     if (!thing_exists(boxtng) || (box_thing_to_special(boxtng) != SpcKind_TrnsfrCrtr) ) {
         ERRORMSG("Invalid transfer box object!");
         return;
     }
-    dungeon = get_players_num_dungeon(plyr_idx);
     // Check if 'things' are correct
     if (!thing_exists(transftng) || !thing_is_creature(transftng) || (transftng->owner != plyr_idx)) {
         ERRORMSG("Invalid transfer creature thing!");

keeperfx/src/thing_creature.c
@@ -722,6 +722,13 @@ TbBool creature_affected_by_spell(const struct Thing *thing, SpellKind spkind)
 
 }
 
+TbBool creature_affected_by_slap(const struct Thing *thing)
+{
+    struct CreatureControl *cctrl;
+    cctrl = creature_control_get_from_thing(thing);
+    return (cctrl->slap_turns != 0);
+}
+
 /**
  * Returns remaining duration of a spell casted on a thing.
  * @param thing The thing which can have spells casted on.
@@ -2858,10 +2865,8 @@ long creature_instance_has_reset(const struct Thing *thing, long inst_idx)
 void get_creature_instance_times(const struct Thing *thing, long inst_idx, long *ritime, long *raitime)
 {
     struct InstanceInfo *inst_inf;
-    struct CreatureControl *cctrl;
     struct Dungeon *dungeon;
     long itime,aitime;
-    cctrl = creature_control_get_from_thing(thing);
     inst_inf = creature_instance_info_get(inst_idx);
     if ((thing->alloc_flags & TAlF_IsControlled) != 0)
     {
@@ -2882,7 +2887,7 @@ void get_creature_instance_times(const struct Thing *thing, long inst_idx, long 
         aitime /= 2;
         itime /= 2;
     } else
-    if (cctrl->slap_turns)
+    if (creature_affected_by_slap(thing))
     {
         aitime = 3 * aitime / 4;
         itime = 3 * itime / 4;

keeperfx/src/thing_creature.h
@@ -135,6 +135,7 @@ struct Room *get_creature_lair_room(const struct Thing *creatng);
 TbBool remove_creature_lair(struct Thing *thing);
 
 TbBool creature_affected_by_spell(const struct Thing *thing, SpellKind spkind);
+TbBool creature_affected_by_slap(const struct Thing *thing);
 void apply_spell_effect_to_thing(struct Thing *thing, SpellKind spell_idx, long spell_lev);
 void terminate_thing_spell_effect(struct Thing *thing, SpellKind spkind);
 void process_thing_spell_effects(struct Thing *thing);

keeperfx/src/thing_stats.c
@@ -546,7 +546,7 @@ long calculate_correct_creature_maxspeed(const struct Thing *thing)
     cctrl = creature_control_get_from_thing(thing);
     crstat = creature_stats_get_from_thing(thing);
     speed = crstat->base_speed;
-    if (cctrl->slap_turns)
+    if (creature_affected_by_slap(thing))
         speed *= 2;
     if (creature_affected_by_spell(thing, SplK_Speed))
         speed *= 2;